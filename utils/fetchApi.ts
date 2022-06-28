f9df8c232aa2631cae5bfb04ebcbe96d927154f9
Precious Adedibu <adedibuprecious@gmail.com>
2022-06-28 02:41:32 +0100

    hi

--- utils/fetchApi.ts
+++ utils/fetchApi.ts
@@ -2,11 +2,21 @@ import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com";
 
+interface Headers {
+  "X-RapidAPI-Host": string;
+  "X-RapidAPI-Key": string | undefined;
+}
+
+const headers: Headers = {
+  "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+  "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
+};
+
 export const fetchApi = async (url: string) => {
   const { data } = await axios.get(url, {
     headers: {
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
+      "X-RapidAPI-Key": `${process.env.NEXT_PUBLIC_RAPID_API_KEY}`,
     },
   });
 
